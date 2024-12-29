9e7e4af068207f1bb7e720828884a6ba9a36ca75
James Baldwin <james.baldwin3232@gmail.com>
2024-12-29 23:47:54 +0000

    Created form to submit and added post method

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const express = require("express")
 const app = express()
 
 app.use(express.urlencoded({extended:false}))
+app.use(express.static('client'))
 
 let exercises = require("./data/exercises.json")
-console.log(exercises)
 
 app.get("/home", function(req, res){
     res.sendFile(__dirname + "/client/home.html")
@@ -15,8 +15,8 @@ app.get("/exercises", function(req,res){
     res.send(exercises)
 })
 
-app.get("/new", function(req, res){
-    res.sendFile(__dirname + "/client/new.html")
+app.post("/new", function(req, res){
+    console.log(req.body)
 })
 
 module.exports = app;
