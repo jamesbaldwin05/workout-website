f87b6c0a4a0f50134a78ba5fc94783a24743fa2d
James Baldwin <james.baldwin3232@gmail.com>
2024-12-31 01:25:48 +0000

    KInd of working form submit

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ const fs = require("fs")
 const app = express()
 
 // Middleware for express to ensure JSON read correctly
-app.use(express.json())
+app.use(express.json()) // for testing purposes when I was using postman
+//app.use(express.urlencoded({extended:true}))
+app.use(express.static('client'))
 
 // Pre written exercises loaded to be used in exercise route
 let exercises = require("./data/exercises.json")
@@ -27,7 +29,7 @@ app.get("/new", function(req, res){
 })
 
 // Post method to add new exercise
-app.post('/submit', (req, res) => {
+app.post('/new', (req, res) => {
     let data = []
     
     // If json file is currently not empty then the current file is loaded to ensure no data lost/written over with fs.writeFileSync
