91648f48af2055bbbe598d078a6b7ff4aa37b388
James Baldwin <james.baldwin3232@gmail.com>
2025-01-01 21:24:55 +0000

    Finished first table

--- client/table.js
+++ client/table.js
@@ -1,1 +1,36 @@
-console.log("AAAA")
+document.addEventListener("DOMContentLoaded", buildTables)
+
+function buildTables() {
+    fetch("http://localhost:3000/exercises")
+        .then(response => response.json())
+        .then(data => {
+            let tableBody = document.querySelector("#exerciseTable tbody")
+
+            data.forEach(item => {
+                let row = document.createElement("tr")
+                
+                let name = document.createElement("td")
+                name.textContent = item.name
+                row.appendChild(name)
+
+                let muscle = document.createElement("td")
+                muscle.textContent = item.muscle
+                row.appendChild(muscle)
+
+                let difficulty = document.createElement("td")
+                difficulty.textContent = item.difficulty
+                row.appendChild(difficulty)
+
+                let equipment = document.createElement("td")
+                equipment.textContent = item.equipment
+                row.appendChild(equipment)
+
+                let imageHead = document.createElement("td")
+                let image = document.createElement("img")
+                image.src = item.image
+                imageHead.appendChild(image)
+                row.appendChild(imageHead)
+                tableBody.appendChild(row)
+            })
+        })
+}
